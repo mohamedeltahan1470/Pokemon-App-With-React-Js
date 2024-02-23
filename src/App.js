8a08480fd84cfba4af6c90775c8a0ee87a9f020c
mohamedeltahan1470 <mohamedeltahan1470@gmail.com>
2024-02-23 14:42:09 +0200

    Pokemon-App-with-ReactJs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+import PokemonList from "./component/PokemonList";
+import Pagenation from "./component/pagenation";
+const App = () => {
+  const [pokemon, setpokemon] = useState([]);
+  const [loading, setloading] = useState(true);
+  const [CurrentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
   );
-}
+  const [NextPageUrl, setNextPageUrl] = useState();
+  const [PrePageUrl, setPrePageUrl] = useState();
+  useEffect(() => {
+    let cancel;
+    setloading(true);
+    axios
+      .get(CurrentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((response) => {
+        setpokemon(response.data.results.map((p) => p.name));
+        setloading(false);
+        setNextPageUrl(response.data.next);
+        setPrePageUrl(response.data.previous);
+      })
+      .catch((error) => {
+        console.log(error);
+        setloading(false);
+      });
+    return () => {
+      cancel();
+    };
+  }, [CurrentPageUrl]);
+  if (loading) return "loading...";
+  function GoToNextPage() {
+    setCurrentPageUrl(NextPageUrl);
+  }
+  function GoToPrePage() {
+    setCurrentPageUrl(PrePageUrl);
+  }
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagenation
+        GoToNextPage={NextPageUrl ? GoToNextPage : null}
+        GoToPrePage={PrePageUrl ? GoToPrePage : null}
+      />
+    </>
+  );
+};
 
 export default App;
